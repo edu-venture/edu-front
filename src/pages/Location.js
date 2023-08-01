f663b2d5ab56776e247fc3669f2c48a453ff0f57
1three <1_three@naver.com>
2023-08-01 20:48:32 +0900

    feat: 실시간 차량 위치 페이지 수정

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,26 +1,28 @@
-import { Box } from "@mui/material";
-import React, { Suspense } from "react";
-import LocationLoading from "../components/Location/LocationLoading";
+import React from "react";
+import styled from "styled-components";
+import Title from "../components/Title";
+import Map from "../components/Location/Map";
 
-const LocationContent = React.lazy(() =>
-  import("../components/Location/LocationContent")
-);
-const textBox = {
-  position: "relative",
-  left: "50px",
-  top: "10px",
+const LocationContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: "relative";
+`;
+
+const styles = {
+  titleContainer: {
+    padding: "20px 0px 20px 50px",
+  },
 };
+
 const Location = () => {
   return (
-    <>
-      <Box sx={textBox}>
-        <h2>실시간</h2>
-        <h1>차량 위치</h1>
-      </Box>
-      <Suspense fallback={<LocationLoading />}>
-        <LocationContent />
-      </Suspense>
-    </>
+    <LocationContainer>
+      <div style={styles.titleContainer}>
+        <Title subtitle="실시간" title="차량 위치" />
+      </div>
+      <Map />
+    </LocationContainer>
   );
 };
 
