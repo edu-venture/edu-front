53b1c7ce6e8ceff8617dbd45bc43e363da3ccd28
1three <1_three@naver.com>
2023-08-04 16:52:16 +0900

    feat: 실시간 차량 위치 화면 수정

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Title from "../components/Title";
-import Kim from "../components/Location/Kim";
+import Map from "../components/Location/Map";
 
 const LocationContainer = styled.div`
   width: 100%;
@@ -21,7 +21,7 @@ const Location = () => {
       <div style={styles.titleContainer}>
         <Title subtitle="실시간" title="차량 위치" />
       </div>
-      <Kim />
+      <Map />
     </LocationContainer>
   );
 };
