bdde7979e40da46fe03aebbbb74f4b4c0543cfd0
1three <1_three@naver.com>
2023-08-18 09:39:58 +0900

    feat: 공지사항 CRUD

---
+++ src/pages/Notice.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NoticeSection from "../components/Notice/NoticeSection";
+import NoticeList from "../components/Notice/NoticeList";
+
+const Notice = () => {
+  return (
+    <>
+      <NoticeSection>
+        <NoticeList></NoticeList>
+      </NoticeSection>
+    </>
+  );
+};
+
+export default Notice;
