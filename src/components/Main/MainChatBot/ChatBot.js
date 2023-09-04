59ce7a2f977d2a85b2f3f08e4a8141532686a692
1three <1_three@naver.com>
2023-09-04 14:11:47 +0900

    feat: 챗봇 입장 메시지 기능

--- src/components/Main/MainChatBot/ChatBot.js
+++ src/components/Main/MainChatBot/ChatBot.js
@@ -4,6 +4,17 @@ import ChatBotWindow from "./ChatBotWindow/ChatBotWindow";
 import SockJS from "sockjs-client";
 import { Stomp } from "@stomp/stompjs";
 
+const formatTime = () => {
+  const time = new Date();
+  let hour = time.getHours();
+  const minute = String(time.getMinutes()).padStart(2, "0");
+  const ampm = hour >= 12 ? "오후" : "오전";
+  hour = hour % 12 || 12;
+  hour = String(hour).padStart(2, "0");
+
+  return `${ampm} ${hour}:${minute}`;
+};
+
 const ChatBot = () => {
   const [showWindow, setShowWindow] = useState(false);
   const [roomId, setRoomId] = useState(null);
@@ -14,13 +25,22 @@ const ChatBot = () => {
     const newRoomId = Math.floor(Math.random() * 1000000).toString();
     setRoomId(newRoomId);
 
-    let socket = new SockJS("http://192.168.0.29:8081/ws");
+    let socket = new SockJS("http://192.168.0.216:8081/ws");
     const newStompClient = Stomp.over(() => socket);
     setStompClient(newStompClient);
 
     newStompClient.connect({}, function (frame) {
       console.log("Connected: " + frame);
       setConnected(true);
+
+      // 메시지를 보내는 방식을 handleSendMessage 함수와 유사하게 설정
+      if (newStompClient && newRoomId) {
+        newStompClient.send(
+          `/app/sendMessage/${newRoomId}`,
+          {},
+          JSON.stringify("입장")
+        );
+      }
     });
   };
 
